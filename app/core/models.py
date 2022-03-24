a21fee6d66ab16c657b94a9fcfbddeced8776471
Ivan Buruyane <ivanburuyane@Ivans-MacBook-Pro.local>
2022-03-24 20:37:59 +0200

    added email normalization

--- app/core/models.py
+++ app/core/models.py
@@ -8,7 +8,7 @@ from django.contrib.auth.models import (
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -10,3 +10,10 @@ def test_create_user_with_email_successful():
     user = get_user_model().objects.create_user(email=email, password=password)
     assert user.email == email
     assert user.check_password(password) is True
+
+
+@pytest.mark.django_db
+def test_new_user_email_normalized():
+    email = "test@LonDOnaPPDev.cOm"
+    user = get_user_model().objects.create_user(email=email, password="123")
+    assert user.email == email.lower()
