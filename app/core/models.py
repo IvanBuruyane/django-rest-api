f3f407a6c587371e4eb83ac84005c4c77be75e0a
Ivan Buruyane <ivanburuyane@Ivans-MacBook-Pro.local>
2022-03-24 21:40:19 +0200

    Added create_superuser function

--- app/core/models.py
+++ app/core/models.py
@@ -3,18 +3,31 @@ from django.contrib.auth.models import (
     AbstractBaseUser,
     BaseUserManager,
     PermissionsMixin,
+    User,
 )
 from django.core.validators import validate_email
+from typing import Optional, Any
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
+    def create_user(
+        self, email: Optional[str], password: Optional[str] = None, **extra_fields
+    ) -> Any:
         normalized_email = self.normalize_email(email)
         validate_email(normalized_email)
         user = self.model(email=normalized_email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
+
+    def create_superuser(
+        self, email: Optional[str], password: Optional[str] = None
+    ) -> Any:
+        superuser = self.create_user(email=email, password=password)
+        superuser.is_superuser = True
+        superuser.is_staff = True
+        superuser.save(using=self._db)
+        return superuser
 
 
 class User(AbstractBaseUser, PermissionsMixin):
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -4,7 +4,7 @@ from django.core.exceptions import ValidationError
 
 
 @pytest.mark.django_db
-def test_create_user_with_email_successful():
+def test_create_user_with_email_successful() -> None:
     """Test creating a new user with an email is successful"""
     email = "test@londonappdev.com"
     password = "Password123"
@@ -14,7 +14,7 @@ def test_create_user_with_email_successful():
 
 
 @pytest.mark.django_db
-def test_new_user_email_normalized():
+def test_new_user_email_normalized() -> None:
     """Test the email for a new user is normalized"""
     email = "test@LonDOnaPPDev.cOm"
     user = get_user_model().objects.create_user(email=email, password="123")
@@ -34,9 +34,21 @@ def test_new_user_email_normalized():
         "abc\ is\”not\valid@domain.com",
         ".test@domain.com",
         "test@domain..com",
+        "",
+        "   ",
     ],
 )
-def test_new_user_invalid_email(invalid_email):
-    """Test creation user with empty email raises error"""
+def test_new_user_invalid_email(invalid_email: str) -> None:
+    """Test creating user with empty email raises error"""
     with pytest.raises(ValidationError):
         get_user_model().objects.create_user(email=invalid_email, password="123")
+
+
+@pytest.mark.django_db
+def test_super_user_can_be_created() -> None:
+    """Test creating a superuser"""
+    superuser = get_user_model().objects.create_superuser(
+        email="superuser@londonappdev.com", password="123"
+    )
+    assert superuser.is_superuser is True
+    assert superuser.is_staff is True
