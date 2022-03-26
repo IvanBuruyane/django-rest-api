83bee6b4af2e41b780ec7e77dd79a81897004309
Ivan Buruyane <ivanburuyane@Ivans-MacBook-Pro.local>
2022-03-26 14:08:42 +0200

    Added admin pages

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -12,3 +12,21 @@ def test_users_listed(setup_admin, client):
 
     assert r.status_code == 200
     assert user.name in str(r.content) and user.email in str(r.content)
+
+
+@pytest.mark.django_db
+def test_user_change_page(setup_admin, client):
+    """Test that the user edit page works"""
+    user = setup_admin["user"]
+    url = reverse("admin:core_user_change", args=[user.id])
+    r = client.get(url)
+    assert r.status_code == 200
+
+
+@pytest.mark.django_db
+def test_create_user_page(setup_admin, client):
+    """Test that the create user page works"""
+    user = setup_admin["user"]
+    url = reverse("admin:core_user_add")
+    r = client.get(url)
+    assert r.status_code == 200
